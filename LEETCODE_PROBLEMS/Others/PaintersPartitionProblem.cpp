d5153834ffec5a9c1f55edf82f3c7316552ba9c2
Anjali Ray <rayyyanjali@gmail.com>
2025-12-11 00:15:59 +0530

    Painters Partition Problem solved

---
+++ LEETCODE_PROBLEMS/Others/PaintersPartitionProblem.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+bool isValid(vector<int> arr,int n,int m,int maxAllowed){
+    int painter=1, time=0;
+    for(int i=0;i<arr.size();i++){
+        if(arr[i]>maxAllowed)   return false;
+        if(time+arr[i]<=maxAllowed) time+=arr[i];
+        else{
+            painter++;
+            time=arr[i];
+        }
+    }
+    if(painter<=m)  return true;
+    else return false;
+}
+int minTimeTaken(vector<int>& nums,int n,int m){
+    int maxTime=0;
+    //Calculating the maximum time required
+    for(int i=0;i<nums.size();i++){
+        maxTime+=nums[i];
+    }
+    if(m==1) return maxTime;
+    int st=0,end=maxTime,mid,ans=-1;
+    while(st<end){
+        mid=st+(end-st)/2;
+        if(isValid(nums,n,m,mid)){
+            ans=mid;
+            end=mid-1;
+        }
+        else{
+            st=mid+1;
+        }
+    }
+    return ans;
+}
+int main() {
+    vector<int> a={40,30,10,20};
+    int n=4,m=2;
+    cout<<minTimeTaken(a,n,m);
+    return 0;
+}
