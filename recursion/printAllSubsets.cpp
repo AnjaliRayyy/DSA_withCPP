092ded5ee25b7cc6219e3d89029257b87b1a8433
Anjali Ray <rayyyanjali@gmail.com>
2026-01-01 20:16:05 +0530

    Displaying all subsets using recursion

---
+++ recursion/printAllSubsets.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+void printSubsets(vector<int> arr,vector<int> &ans,int i){
+    if(i==arr.size()){
+        for(int val: ans)
+        cout<<val<<" ";
+        cout<<endl;
+        return;
+    }
+    ans.push_back(arr[i]);
+    printSubsets(arr,ans,i+1);
+    ans.pop_back();
+    printSubsets(arr,ans,i+1);
+}
+int main() {
+    vector<int> set = {1, 2, 3};
+    vector<int> ans;
+    printSubsets(set,ans,0);
+    return 0;
+}
